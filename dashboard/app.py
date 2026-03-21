d626e5330243d359204c4bb64ef05271ce986d6b
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-03-21 14:42:15 -0300

    exploracion del df de sentimientos con filtros, notificacion de carga del boton y traduccion al igles

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from custom import personalizar, COLORS
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="Newsense",
@@ -27,17 +28,73 @@ def show_metrics(df):
             st.metric(label="", value=df.values[i])
 
 
+def show_chart(df):
+    fig = px.pie(
+        values=df.values,
+        names=df.index,
+        color=df.index,
+        color_discrete_map={
+            "positive": "#00C48C",
+            "negative": "#FF4B4B",
+            "neutral": "#A0A0A0",
+        },
+    )
+    fig.update_layout(
+        title="Pie chart",
+    )
+
+    st.plotly_chart(fig, width="stretch")
+
+
+def show_articles(df_sentiments):
+    df_sentiments["score"] = round(df_sentiments["score"], 2)
+    # Filtro 1 — sentimiento
+    sentimientos = st.multiselect(
+        "Filter by sentiment",
+        options=["positive", "negative", "neutral"],
+        default=["positive", "negative", "neutral"],
+    )
+
+    # Filtro 2 — búsqueda por palabra
+    busqueda = st.text_input("Search in titles")
+
+    # Filtro 3 — ordenar por score
+    orden = st.selectbox(
+        "Sort by score", options=["Highest to lowest", "Lowest to highest"]
+    )
+    df_filtrado = df_sentiments[df_sentiments["sentiment"].isin(sentimientos)]
+    df_filtrado = df_filtrado[
+        df_filtrado["title"].str.contains(busqueda, case=False, na=False)
+    ]
+    if orden == "Highest to lowest":
+        df_filtrado = df_filtrado.sort_values(by="score", ascending=False)
+    elif orden == "Lowest to highest":
+        df_filtrado = df_filtrado.sort_values(by="score", ascending=True)
+
+    st.write(df_filtrado)
+
+
 def main():
     header()
-    topic = st.text_input(label="Ingrese una tematica")
-    if st.button("Analizar"):
-        requests.get("http://localhost:8000/news", params={"topic": topic})
-        response = requests.post("http://localhost:8000/analyze", json={"topic": topic})
-        analyze_data = response.json()
-        df_sentiments = pd.DataFrame(analyze_data["results"])
+    topic = st.text_input(label="Enter a topic")
+    if st.button("Analyze"):
+        with st.spinner("Fetching and analyzing news..."):
+            requests.get("http://localhost:8000/news", params={"topic": topic})
+            response = requests.post(
+                "http://localhost:8000/analyze", json={"topic": topic}
+            )
+            analyze_data = response.json()
+            st.session_state["results"] = analyze_data["results"]  # guardás
+
+    if "results" in st.session_state:
+        df_sentiments = pd.DataFrame(st.session_state["results"])
         df_result = df_sentiments["sentiment"].value_counts()
         st.divider()
         show_metrics(df_result)
+        st.markdown("")
+        show_chart(df_result)
+        st.markdown("")
+        show_articles(df_sentiments)
 
 
 if __name__ == "__main__":
